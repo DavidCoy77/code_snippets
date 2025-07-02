689b9ef9d07d97e2375814c1a2d5ad7216d9608c
DavidCoy77 <143979402+DavidCoy77@users.noreply.github.com>
2025-07-02 17:05:12 -0400

    upload corey schaffer code

--- Python/MultiProcessing/multiprocessing-demo.py
+++ Python/MultiProcessing/multiprocessing-demo.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import concurrent.futures
 import time
 
--- Python/MultiProcessing/process_images_serial.py
+++ Python/MultiProcessing/process_images_serial.py
@@ -33,11 +33,23 @@ img_names = [
 size = (1200, 1200)
 
 def main():
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(process_image, img_names)
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     executor.map(process_image, img_names)
 
+    # t2 = time.perf_counter()
+    # print(f'Finished in {t2-t1} seconds')
+    
+    for img_name in img_names:
+        img = Image.open(img_name)
+
+        img = img.filter(ImageFilter.GaussianBlur(15))
+
+        img.thumbnail(size)
+        img.save(f'processed/{img_name}')
+        print(f'{img_name} was processed...')
+    
     t2 = time.perf_counter()
     print(f'Finished in {t2-t1} seconds')
-
+    
 if __name__ == '__main__':
     main()
